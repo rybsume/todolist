217b1dd4f7106b2657510624c164ca61adb8708e
Łukasz <lukaszrybski88@o2.pl>
2020-04-30 20:12:40 +0200

    complete 2nd episdoe: add EJS, create first template

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 //jshint esversion:6
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get("/", function (req, res) {
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
   var today = new Date();
   var currentDay = today.getDay();
-  if (currentDay == 6 || currentDay == 0) {
-    res.send("<h1>Yay! It's weekend!</h1>");
-  } else {
-    res.sendFile(__dirname+"/index.html");
+  var day = "";
+  switch (currentDay) {
+    case 1:
+      day = 'Monday';
+      break;
+    case 2:
+      day = 'Tuesday';
+      break;
+    case 3:
+      day = 'Wednesday';
+      break;
+    case 4:
+      day = 'Thursday';
+      break;
+    case 5:
+      day = 'Friday';
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    case 0:
+      day = "Sunday";
+      break;
+    default:
+      day = 'Oops! Looks like the Code is not working :(';
+      console.log("error: day is undefined");
   }
-});
+  res.render('list', {dayName: day});
+}
+);
 
 app.listen(3000, function () {
   console.log("Server started on port 3000.");
