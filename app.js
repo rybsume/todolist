77d7432ff01a55b5b9c400d41c48374095d56e08
Łukasz <lukaszrybski88@o2.pl>
2020-04-30 19:33:06 +0200

    complete first part of tutorial

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
-  res.send("Hello");
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+  if (currentDay == 6 || currentDay == 0) {
+    res.send("<h1>Yay! It's weekend!</h1>");
+  } else {
+    res.sendFile(__dirname+"/index.html");
+  }
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
   console.log("Server started on port 3000.");
 });
